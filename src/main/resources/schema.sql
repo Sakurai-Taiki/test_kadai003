32eb354f47db68d726f5a1a74c49c9d2f53e0af1
Sakurai-Taiki <bass.taiki0707@gmail.com>
2024-12-07 09:15:59 +0900

    Add files via upload

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -74,11 +74,12 @@ CREATE TABLE IF NOT EXISTS reservations
 
 --お気に入りのテーブル
 CREATE TABLE IF NOT EXISTS favorites (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT NOT NULL,
-    house_id INT NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users(id),
-    FOREIGN KEY (house_id) REFERENCES houses(id)
+     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+     house_id INT NOT NULL,
+     user_id INT NOT NULL,    
+     created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+     updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+     UNIQUE (house_id, user_id),
+     FOREIGN KEY (house_id) REFERENCES houses (id),
+     FOREIGN KEY (user_id) REFERENCES users (id)  
 );
